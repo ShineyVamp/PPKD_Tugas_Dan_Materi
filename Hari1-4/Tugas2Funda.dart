6f9a2493a5887a080ae5ed8fb89cd95771532b3c
ShineyVamp <shineyvampire@gmail.com>
2026-07-20 08:23:10 +0700

    Ganti angka uts

--- Hari1-4/Tugas2Funda.dart
+++ Hari1-4/Tugas2Funda.dart
@@ -1,6 +1,6 @@
 void main() {
   int UAS = 80;
-  int UTS = 90;
+  int UTS = 70;
   double kehadiran = 0.90;
   double rata_rata = (UTS + UAS)/2; 
   if (rata_rata >= 70 && kehadiran >= 0.75 && !(UTS <= 60 || UAS <= 60)){
